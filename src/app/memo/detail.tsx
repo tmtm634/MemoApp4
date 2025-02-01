f9f6c650ef944150ab674b835cd5988802b9a6d3
tmtm634 <68714194+tmtm634@users.noreply.github.com>
2025-02-01 13:00:51 +0900

    edit

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -8,17 +8,22 @@ import Icon from '../../components/Icon'
 import { auth, db } from '../../config'
 import { type Memo } from '../../../types/memo'
 
-const handlePress = (): void => {
-    router.push('/memo/edit')
+const handlePress = (id: string): void => {
+    router.push({
+        pathname: '/memo/edit',
+        params: {
+            id
+        }
+    })
 }
 
 const Detail = (): JSX.Element => {
-    const { id } = useLocalSearchParams()
+    const id = String(useLocalSearchParams().id)
     console.log(id)
     const [memo, setMemo] = useState<Memo | null>(null)
     useEffect(() => {
         if (auth.currentUser === null) { return }
-        const ref = doc(db, `users/${auth.currentUser.uid}/memos/`, String(id))
+        const ref = doc(db, `users/${auth.currentUser.uid}/memos/`, id)
         const unsubscribe = onSnapshot(ref, (memoDoc) => {
             const { bodyText, updatedAt } = memoDoc.data() as Memo
             setMemo({
@@ -42,7 +47,7 @@ const Detail = (): JSX.Element => {
                 </Text>
 
             </ScrollView>
-            <CircleButton onPress={handlePress} style={{ top: 60, bottom: 'auto' }}>
+            <CircleButton onPress={() => handlePress(id)} style={{ top: 60, bottom: 'auto' }}>
                 <Icon name='pencil' size={40} color={'#ffffff'} />
             </CircleButton>
 
--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,23 +1,56 @@
 import {
-    View, TextInput, StyleSheet, KeyboardAvoidingView
-
+    View, TextInput, StyleSheet, KeyboardAvoidingView, Alert
 } from 'react-native'
-import { router } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
+import { useEffect, useState } from 'react'
+import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore'
 
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
+import { auth, db } from '../../config'
 
-const handlePress = (): void => {
-    router.back()
+const handlePress = (id: string, bodyText: string): void => {
+    if (auth.currentUser === null) { return }
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos/`, id)
+    setDoc(ref, {
+        bodyText,
+        updatedAt: Timestamp.fromDate(new Date())
+    })
+        .then(() => {
+            router.back()
+        })
+        .catch((error) => {
+            console.error(error)
+            Alert.alert('メモの更新に失敗しました')
+        })
 }
 
 const Edit = (): JSX.Element => {
+    const id = String(useLocalSearchParams().id)
+    const [bodyText, setBodyText] = useState('')
+    useEffect(() => {
+        if (auth.currentUser === null) { return }
+        const ref = doc(db, `users/${auth.currentUser.uid}/memos/`, id)
+        getDoc(ref)
+            .then((docRef) => {
+                const RemoteBodyText = docRef.data()?.bodyText
+                setBodyText(RemoteBodyText)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }, [])
     return (
         <KeyboardAvoidingView behavior='height' style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value={'買い物\nリスト'} />
+                <TextInput
+                    multiline
+                    style={styles.input}
+                    value={bodyText}
+                    onChangeText={(text) => setBodyText(text)}
+                />
             </View>
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={() => handlePress(id, bodyText)}>
                 <Icon name='check' size={40} color={'#ffffff'} />
             </CircleButton>
         </KeyboardAvoidingView>
@@ -29,12 +62,13 @@ const styles = StyleSheet.create({
         flex: 1
     },
     inputContainer: {
-        paddingVertical: 32,
-        paddingHorizontal: 27,
+
         flex: 1
 
     },
     input: {
+        paddingVertical: 32,
+        paddingHorizontal: 27,
         flex: 1,
         textAlignVertical: 'top',
         fontSize: 16,
